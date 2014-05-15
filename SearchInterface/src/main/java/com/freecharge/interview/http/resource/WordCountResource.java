4b62fb53de449febf2a56b7fc97b8c944bef0653
Manoj Kumar <harish224227@gmail.com>
2014-05-15 18:25:55 +0530

    Integrating the indexer query

--- SearchInterface/src/main/java/com/freecharge/interview/http/resource/WordCountResource.java
+++ SearchInterface/src/main/java/com/freecharge/interview/http/resource/WordCountResource.java
@@ -1,6 +1,7 @@
 package com.freecharge.interview.http.resource;
 
 import com.freecharge.interview.http.model.CountModel;
+import com.freecharge.interview.index.WordIndex;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,9 +12,13 @@ import javax.ws.rs.core.MediaType;
 @Path("/")
 public class WordCountResource {
 
+    private WordIndex index = WordIndex.getInstance();
+
     @GET
     @Produces(value = MediaType.APPLICATION_JSON)
     public CountModel getCountFor(@QueryParam("q") String word){
-        return new CountModel();
+        CountModel countModel = new CountModel();
+        countModel.setCount(index.getCountFor(word));
+        return countModel;
     }
 }
